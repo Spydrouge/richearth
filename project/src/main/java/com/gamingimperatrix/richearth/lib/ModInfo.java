308aec41b29e2bc87ec6137ecd4fc50ddab8c66c
Tuli Ashuliva <Tuli Ashuliva>
2016-11-17 19:40:54 +0800

    Created the getVersionFromJava function.

--- project/src/main/java/com/gamingimperatrix/richearth/lib/ModInfo.java
+++ project/src/main/java/com/gamingimperatrix/richearth/lib/ModInfo.java
@@ -7,9 +7,7 @@ public class ModInfo {
 
 	public static final String MOD_ID = "richearth";
 	public static final String MOD_NAME = "RichEarth";
-	public static final String VERSION = "1.0.0";
-	public static final String PATCH_VERSION = "0";
-	public static final String MOD_VERSION = VERSION + "." + PATCH_VERSION;
+	public static final String MOD_VERSION = "1.0.0.0";
 	
 	public static final String FACTORY = "";
 	public static final Logger LOG = LogManager.getLogger(MOD_NAME);
